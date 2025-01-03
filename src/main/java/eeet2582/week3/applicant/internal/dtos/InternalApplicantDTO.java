e212ac1ba4213e4d493c7b73b5d2fa6971750b9e
LtMtrunG <s3927071@rmit.edu.vn>
2025-01-03 20:02:20 +0700

    Add id to InternalApplicantDTO for deleting purpose on FE

--- src/main/java/eeet2582/week3/applicant/internal/dtos/InternalApplicantDTO.java
+++ src/main/java/eeet2582/week3/applicant/internal/dtos/InternalApplicantDTO.java
@@ -7,11 +7,13 @@ import java.util.List;
 
 @Getter
 public class InternalApplicantDTO {
+    private final Long id;
     private final String name;
     private final int age;
     private final List<String> culturalClasses;
 
     public InternalApplicantDTO(Applicant applicant) {
+        this.id = applicant.getId();
         this.name = applicant.getName();
         this.age = applicant.getAge();
         this.culturalClasses = applicant.getCulturalClasses();
